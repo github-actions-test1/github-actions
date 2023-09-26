467ee90d3dfa6f0b24281d4bb8309d22704b4cca
Sriram Yepuganti <sriram.yepuganti@tigeranalytics.com>
2023-09-26 20:05:10 +0530

    latest

--- backend.tf
+++ backend.tf
@@ -13,6 +13,10 @@ terraform {
     storage_account_name = "testtfstateoidc"
     resource_group_name = "free-res"
     container_name = "tfstate"
+    client_id = "8a273953-5184-4899-9410-d01680b15150"
+    tenant_id = "44f956ed-ca61-4b64-b36e-4ad00d218ed1"
+    subscription_id = "429ab725-ab09-4085-8ed9-1db644407c13"
+
   }
 }
 
