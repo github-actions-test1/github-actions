d16342f330d3b9133b33bdd9af73cf1284266c44
Sriram Yepuganti <sriram.yepuganti@tigeranalytics.com>
2023-09-26 19:36:14 +0530

    added

--- backend.tf
+++ backend.tf
@@ -10,6 +10,9 @@ terraform {
     key              = "terraform.tfstate"
     use_oidc         = true
     use_azuread_auth = true
+    storage_account_name = "testtfstateoidc"
+    resource_group_name = var.resource_group_name
+    container_name = "tfstate"
   }
 }
 
